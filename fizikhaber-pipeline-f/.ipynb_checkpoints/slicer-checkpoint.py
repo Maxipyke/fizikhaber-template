d9e15671f2770dfcf792517e58c111412b9d0c1c
Erdem Gozay <erdemgozay2004@gmail.com>
2024-04-20 19:35:48 +0300

    added-html-template

---
+++ fizikhaber-pipeline-f/.ipynb_checkpoints/slicer-checkpoint.py
@@ -0,0 +1,43 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "ec792f91-180a-4edf-ba04-db30a0d77c1b",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#This section is for slicing the whole text according to its paragraphs."
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "36c40d02-416e-416b-bc97-ecb90123a405",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.2"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
---
+++ fizikhaber-pipeline-f/slicer.py
@@ -0,0 +1,46 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "ec792f91-180a-4edf-ba04-db30a0d77c1b",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#This section is for slicing the whole text according to its paragraphs."
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "89507f86-e8df-46ed-aee5-f11616559e7e",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def slicer(paragraph: str):\n",
+    "    "
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.2"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
